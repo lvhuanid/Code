aede09ef408e625f21e41b3730d56e906db25f40
Huan <lvhuanid@163.com>
2023-04-23 19:36:41 +0800

    drag & drop

---
+++ drag & drop/script.js
@@ -0,0 +1,18 @@
+
+const boxes = document.querySelectorAll(".box")
+const image = document.querySelector(".image")
+
+boxes.forEach((box) => {
+    box.addEventListener("dragover", (e) => {
+        e.preventDefault();
+        box.classList.add("hover")
+    })
+    box.addEventListener("dragleave", () => {
+        box.classList.remove("hover")
+    })
+
+    box.addEventListener("drop", ()=> {
+        box.appendChild(image)
+        box.classList.remove("hover")
+    })
+})
